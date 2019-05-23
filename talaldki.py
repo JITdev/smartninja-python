c35154e6f5739ae1165012914d60b58c9ccb5883
Roland Kainrath <justintimedev@gmail.com>
2019-05-23 11:58:03 +0200

    Hazi feladat

--- talaldki.py
+++ talaldki.py
@@ -10,16 +10,16 @@ def play_game(tries = 5, end = 5):
     """
     guess = 0
     secret = random.randint(1, end)
-    score_list = []
+    guess_list = []
 
     for x in range(tries):
         print(f'Az {x + 1}. probalkozas')
         guess = int(input(f"Guess the secret number (between 1 and {end}): "))
-        score_list.append(guess)
+        guess_list.append(guess)
 
         if guess == secret:
-            print("You've guessed it - congratulations! It's number .")
-            set_top_scores(x)
+            print(f"You've guessed it - congratulations! It's number {secret}.")
+            set_top_scores(x+1)
             break
         elif x == tries - 1:
             print(f"You lost, the secret number is: {secret}")
@@ -28,30 +28,31 @@ def play_game(tries = 5, end = 5):
         elif guess < secret:
             print("Your guess is not correct... try something bigger")
 
-    set_score_list(score_list)
+    set_guess_list(guess_list)
 
 
-def set_score_list(score_list: list) -> None:
+def set_guess_list(score_list: list) -> None:
     with open("score_list.txt", "w") as score_file:
         score_file.write(json.dumps(score_list))
 
 def set_top_scores(score: int):
-    """ Az utolso 3 legjobb eredmeny
+    """ Az utolso 3 legjobb eredmeny """
+    with open("score.txt", "r+") as score_file:
+        score_list = json.loads(score_file.read())
+        score_list.append(score)
+        score_list.sort()
+        score_json = json.dumps(score_list[:3])
+        score_file.seek(0)
+        score_file.write(score_json)
 
-    Meg kell nyitni a fajlt, json.loads()
-    score_list valtozoba tegyuk be > adjuk hozza a score valtozot
-    rendezzuk a listat: sort()
-     .write(json.dumps(score_list[:3]))
-    """
-    pass
-
-def get_score_list():
-    return
+def get_guess_list():
+    with open("score_list.txt", "r") as score_file:
+        guesses = score_file.read()
+        print("Top score: " + guesses)
 
 def get_top_scores():
-    raise NotImplementedError
+    with open("score.txt", "r") as score_file:
+        best_scores = score_file.read()
+        print("Top score: " + best_scores)
 
-
-with open("score.txt", "r") as score_file:
-    best_score = score_file.read()
-    print("Top score: " + best_score)
+play_game()
